c1eacdc1b0fcccec9d76f483757ea96e4890996e
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-03 07:28:23 +0100

    restructuring

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+def pascal_triangle(n):
+    ''' Defines the Pascal triangle
+
+        res_idx = The index of the previous sub-list of the result list
+                    initial value = -1 since there is no previous sub-list yet
+        prev = index of the previous value in the res_idx list
+        current = index of the current value in the res_idx list
+    '''
+    if type(n) != int or n <= 0:
+        return []
+
+    result = []
+    res_idx = -1
+    for i in range(1, n + 1):
+        new = []
+        for j in range(i):
+            try:
+                current = j
+                prev = current - 1
+                if prev < 0:
+                    prev = 0
+                    val = 1
+                else:
+                    val = result[res_idx][prev] + result[res_idx][current]
+            except IndexError:
+                val = 1
+            new.append(val)
+        result.append(new)
+        res_idx += 1
+    return result
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,16 @@
+# PASCAL TRIANGLE
+
+### [0-pascal_triangle.py](https://github.com/JamesRaphaelJRC/alx-interview/blob/main/0-pascal_triangle.py)
+Returns a list of lists of integers representing the Pascal's triangle of n.
+
+### [0-main.py](https://github.com/JamesRaphaelJRC/alx-interview/blob/main/0-main.py)
+Prints the triangle in this format:
+###### [1]
+###### [1,1]
+###### [1,2,1]
+###### [1,3,3,1]
+###### [1,4,6,4,1]
+
+
+### USAGE
+    ./0-main.py
