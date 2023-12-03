65f61e449b0e85c44de4ee6abecf2eb463dacc86
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-03 22:12:58 +0100

    changed print-out format

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -9,7 +9,7 @@ def print_triangle(triangle):
     Print the triangle
     """
     for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+        print(' '.join(map(str, row)))
 
 
 if __name__ == "__main__":
