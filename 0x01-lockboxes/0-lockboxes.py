2a1376b8a3f902d3499bf254a3b4670ab7c144c5
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-08 00:22:32 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' Defines a function 'canUnlockAll' '''
+
+
+def canUnlockAll(boxes):
+    ''' Returns True if all boxes can be opened and False otherwise '''
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True
