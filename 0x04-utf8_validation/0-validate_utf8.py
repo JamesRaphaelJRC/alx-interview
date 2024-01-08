8418eeb29ec71d9f872c386a6a3daf7c54b18459
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-08 01:02:58 +0100

    task 1 update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,28 +23,29 @@ def validUTF8(data: List[int]) -> bool:
                     mask_n_byte = mask_n_byte >> 1
 
                 # If number of bytes did not increase then it has 1 byte
-                # which is the same we are counting so no need to check next bytes
-                # for current character
+                # which is the same we are counting so no need to check
+                # next bytes for current character
                 if leading_bits_count == 0:
                     continue
 
                 # A character in UTF-8 can be 1 to 4 bytes long
-                # But 1 byte characters start in 0 so leading_bits_count should never
-                # be 1
+                # But 1 byte characters start in 0 so leading_bits_count
+                # should never be 1
                 if leading_bits_count == 1 or leading_bits_count > 4:
                     return False
 
             else:
-                # Every byte that is not the first byte of a character should start
-                # with 10, otherwise is not valid
+                # Every byte that is not the first byte of a character
+                # should start with 10, otherwise is not valid
                 if not (byte & mask_1 and not (byte & mask_2)):
-                        return False
+                    return False
 
-            # If bytes of character are valid, then the count will decrease with
-            # each byte until a new character starts
+            # If bytes of character are valid, then the count will
+            # decrease with each byte until a new character starts
             leading_bits_count -= 1
 
-        # All characters were verified correctly with their proper byte count
+        # All characters were verified correctly with their proper
+        # byte count
         return leading_bits_count == 0
     except TypeError as e:
         return False
