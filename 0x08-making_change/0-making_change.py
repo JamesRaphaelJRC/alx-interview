64d1bf63ad1576a5b544ec7e7975e1002fef68b0
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-02-01 12:17:02 +0100

    initial commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Main module '''
+
+
+def makeChange(coins, total):
+    ''' Returns the fewest number of coins needed to meet 'total'
+    '''
+    if total <= 0:
+        return 0
+
+    # Initialize an array to store the minimum number of coins needed
+    # for each amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0  # You need 0 coins to make change for 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
