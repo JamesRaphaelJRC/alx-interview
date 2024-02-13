cfa4363994926f7bb2a3561b47985e42833e070a
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-02-13 10:18:08 +0100

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,27 +4,29 @@
 
 def is_prime(n):
     """ Checks if a number given n is a prime number """
-    return all(n % i != 0 for i in range(2, int(n ** 0.5) + 1))
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
 
 
 def calculate_primes(n, primes):
     """ Calculate all primes """
     top_prime = primes[-1]
-    primes.extend(i for i in range(top_prime + 1, n + 1) if is_prime(i))
+    if n > top_prime:
+        for i in range(top_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
 
 
 def isWinner(x, nums):
     """
-    Arguments:
-    x - number of rounds.
-    nums - an array of n
-
-    Return:
-        - name of the player that won the most rounds
-        - If winner cannot be determined, return None
-
-    Assumption:
-        n and x will not be larger than 10000
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
     """
 
     players_wins = {"Maria": 0, "Ben": 0}
@@ -33,10 +35,14 @@ def isWinner(x, nums):
 
     calculate_primes(max(nums), primes)
 
-    for n in nums:
-        sum_options = sum(i != 0 and i <= n for i in primes[:n + 1])
+    for round in range(x):
+        sum_options = sum((i != 0 and i <= nums[round])
+                          for i in primes[:nums[round] + 1])
 
-        winner = "Maria" if sum_options % 2 else "Ben"
+        if (sum_options % 2):
+            winner = "Maria"
+        else:
+            winner = "Ben"
 
         if winner:
             players_wins[winner] += 1
